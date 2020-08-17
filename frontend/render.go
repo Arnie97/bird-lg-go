b605461154691d993e68fde4d78c4869fe2fcbf0
Arnie97 <arnie97@gmail.com>
2020-08-17 10:52:16 +0800

    frontend: customize stylesheets

--- frontend/render.go
+++ frontend/render.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"regexp"
 	"sort"
@@ -90,17 +91,17 @@ func summaryTable(data string, serverName string) string {
 	stringsSplitted := strings.Split(strings.TrimSpace(data), "\n")
 	if len(stringsSplitted) <= 1 {
 		// Likely backend returned an error message
-		result = "<pre>" + strings.TrimSpace(data) + "</pre>"
+		result = fmt.Sprintf("<pre>%s</pre>", strings.TrimSpace(data))
 	} else {
 		// Draw the table head
-		result += `<table class="table table-striped table-bordered table-sm">`
+		result += `<table class="table table-hover table-bordered table-sm">`
 		result += `<thead>`
 		for _, col := range strings.Split(stringsSplitted[0], " ") {
 			colTrimmed := strings.TrimSpace(col)
 			if len(colTrimmed) == 0 {
 				continue
 			}
-			result += `<th scope="col">` + colTrimmed + `</th>`
+			result += fmt.Sprintf(`<th scope="col">%s</th>`, colTrimmed)
 		}
 		result += `</thead><tbody>`
 
@@ -141,12 +142,13 @@ func summaryTable(data string, serverName string) string {
 			}
 
 			// Draw the row in red if the link isn't up
-			result += `<tr class="` + (map[string]string{
+			result += fmt.Sprintf(`<tr class="%s">`, map[string]string{
 				"up":      "table-success",
-				"down":    "table-secondary",
+				"down":    "table-warning",
 				"start":   "table-danger",
 				"passive": "table-info",
-			})[row[3]] + `">`
+			}[row[3]])
+
 			// Add link to detail for first column
 			result += `<td><a href="/detail/` + serverName + `/` + row[0] + `">` + row[0] + `</a></td>`
 			// Draw the other cells
