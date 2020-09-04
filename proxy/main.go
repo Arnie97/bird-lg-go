1308d10294182122e5bd9a393afa17a2829fd492
Arnie97 <arnie97@gmail.com>
2020-09-05 04:55:57 +0800

    proxy: automated peering support

--- proxy/main.go
+++ proxy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"net/http"
 	"os"
@@ -54,9 +55,15 @@ type settingType struct {
 	bird6Socket string
 	listen      string
 	allowedIPs  []string
+	peeringConf string
+	templates   string
 }
 
-var setting settingType
+var (
+	setting     settingType
+	peeringConf *Peering
+	templates   []TemplateFile
+)
 
 // Wrapper of tracer
 func main() {
@@ -66,6 +73,8 @@ func main() {
 		"/var/run/bird/bird6.ctl",
 		":8000",
 		[]string{""},
+		"",
+		"templates",
 	}
 
 	if birdSocketEnv := os.Getenv("BIRD_SOCKET"); birdSocketEnv != "" {
@@ -80,18 +89,40 @@ func main() {
 	if AllowedIPsEnv := os.Getenv("ALLOWED_IPS"); AllowedIPsEnv != "" {
 		settingDefault.allowedIPs = strings.Split(AllowedIPsEnv, ",")
 	}
+	if peeringEnv := os.Getenv("BIRDLG_PEERING"); peeringEnv != "" {
+		settingDefault.peeringConf = peeringEnv
+	}
+	if templatesEnv := os.Getenv("BIRDLG_TEMPLATES"); templatesEnv != "" {
+		settingDefault.templates = templatesEnv
+	}
 
 	// Allow parameters to override environment variables
 	birdParam := flag.String("bird", settingDefault.birdSocket, "socket file for bird, set either in parameter or environment variable BIRD_SOCKET")
 	bird6Param := flag.String("bird6", settingDefault.bird6Socket, "socket file for bird6, set either in parameter or environment variable BIRD6_SOCKET")
 	listenParam := flag.String("listen", settingDefault.listen, "listen address, set either in parameter or environment variable BIRDLG_LISTEN")
 	AllowedIPsParam := flag.String("allowed", strings.Join(settingDefault.allowedIPs, ","), "IPs allowed to access this proxy, separated by commas. Don't set to allow all IPs.")
+	peeringParam := flag.String("peering", settingDefault.peeringConf, "peering config file, set either in parameter or environment variable BIRDLG_PEERING")
+	templatesParam := flag.String("templates", settingDefault.templates, "peering config file, set either in parameter or environment variable BIRDLG_TEMPLATES")
 	flag.Parse()
 
 	setting.birdSocket = *birdParam
 	setting.bird6Socket = *bird6Param
 	setting.listen = *listenParam
 	setting.allowedIPs = strings.Split(*AllowedIPsParam, ",")
+	setting.peeringConf = *peeringParam
+	setting.templates = *templatesParam
+
+	if setting.peeringConf != "" {
+		if file, err := os.Open(setting.peeringConf); err != nil {
+			panic(err)
+		} else if err = json.NewDecoder(file).Decode(&peeringConf); err != nil {
+			panic(err)
+		} else {
+			file.Close()
+		}
+		loadTemplates()
+	}
+>>>>>>> ae36c0f... proxy: automated peering support
 
 	// Start HTTP server
 	http.HandleFunc("/", invalidHandler)
@@ -99,5 +130,6 @@ func main() {
 	http.HandleFunc("/bird6", birdIPv6Wrapper)
 	http.HandleFunc("/traceroute", tracerouteIPv4Wrapper)
 	http.HandleFunc("/traceroute6", tracerouteIPv6Wrapper)
+	http.HandleFunc("/peering", peeringWrapper)
 	http.ListenAndServe(*listenParam, handlers.LoggingHandler(os.Stdout, accessHandler(http.DefaultServeMux)))
 }
---
+++ proxy/peering.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+	"time"
+)
+
+type PointOfPresence struct {
+	Name                string `json:"name"`
+	AutonomousSystem    uint32 `json:"asn"`
+	WireGuardEndpoint   string `json:"wg"`
+	WireGuardPrivateKey string `json:"priv,omitempty"`
+	WireGuardPublicKey  string `json:"publ,omitempty"`
+	TunneledIPv4        net.IP `json:"ipv4,omitempty"`
+	TunneledIPv6        net.IP `json:"ipv6,omitempty"`
+	LinkLocalIPv6       net.IP `json:"link,omitempty"`
+	Geolocation         string `json:"loc,omitempty"`
+}
+
+type Communities struct {
+	Latency, Bandwidth uint32
+}
+
+type Peering struct {
+	Alice, Bob PointOfPresence
+	Communities
+}
+
+type TemplateFile struct {
+	Path              string
+	FileName, Content *template.Template
+}
+
+// bob should not tell alice his private key, and vice versa
+func (p *Peering) MaskPrivateKeys() *Peering {
+	p.Alice.WireGuardPrivateKey = "<private key>"
+	p.Bob.WireGuardPrivateKey = "<private key>"
+	return p
+}
+
+func loadTemplates() {
+	filepath.Walk(setting.templates, func(path string, info os.FileInfo, err error) error {
+		var tmpl TemplateFile
+		if err != nil {
+			return err
+		} else if !strings.Contains(info.Name(), "{{") {
+			return nil
+		} else if tmpl.FileName, err = template.New(info.Name()).Parse(info.Name()); err != nil {
+			return err
+		} else if bytes, err := ioutil.ReadFile(path); err != nil {
+			return err
+		} else if tmpl.Content, err = template.New(info.Name()).Parse(string(bytes)); err != nil {
+			return err
+		} else {
+			tmpl.Path = path
+			templates = append(templates, tmpl)
+			return nil
+		}
+	})
+}
+
+func peeringWrapper(httpW http.ResponseWriter, httpR *http.Request) {
+	if peeringConf == nil {
+		invalidHandler(httpW, httpR)
+		return
+	}
+
+	switch httpR.Method {
+	case "GET":
+		resp := *peeringConf
+		json.NewEncoder(httpW).Encode(resp.MaskPrivateKeys())
+	case "POST":
+		resp, err := peeringHandler(httpR.Body)
+		if err != nil {
+			resp = map[string]string{"error": err.Error()}
+		} else {
+			birdReconfigure(setting.birdSocket)
+		}
+		json.NewEncoder(httpW).Encode(resp)
+	}
+}
+
+func peeringHandler(body io.ReadCloser) (interface{}, error) {
+	var (
+		req      *Peering
+		resp     = make(map[string]string)
+		fileName = bytes.NewBuffer(nil)
+		content  = bytes.NewBuffer(nil)
+	)
+	defer body.Close()
+	if err := json.NewDecoder(body).Decode(&req); err != nil {
+		return nil, err
+	}
+
+	// do not trust bob, use local config for alice
+	localConf := &Peering{
+		Alice:       peeringConf.Alice,
+		Bob:         req.Bob,
+		Communities: req.Communities,
+	}
+
+	// this is the only field in alice that bob might change
+	if req.Alice.LinkLocalIPv6.IsLinkLocalUnicast() {
+		localConf.Alice.LinkLocalIPv6 = req.Alice.LinkLocalIPv6
+	}
+
+	// swap alice and bob to generate an example config for the peer AS
+	peerConf := (&Peering{
+		Alice:       localConf.Bob,
+		Bob:         localConf.Alice,
+		Communities: localConf.Communities,
+	}).MaskPrivateKeys()
+
+	for _, tmpl := range templates {
+
+		// local config
+		if err := tmpl.FileName.Execute(fileName, localConf); err != nil {
+			return nil, err
+		} else if file, err := os.OpenFile(filepath.Join(filepath.Dir(tmpl.Path), fileName.String()), os.O_WRONLY|os.O_CREATE, 0644); err != nil {
+			return nil, err
+		} else if err = tmpl.Content.Execute(file, localConf); err != nil {
+			file.Close()
+			os.Remove(file.Name())
+			return nil, err
+		}
+		fileName.Reset()
+
+		// peer config
+		if err := tmpl.FileName.Execute(fileName, peerConf); err != nil {
+			return nil, err
+		} else if err = tmpl.Content.Execute(content, peerConf); err != nil {
+			return nil, err
+		} else {
+			resp[fileName.String()] = content.String()
+		}
+		fileName.Reset()
+		content.Reset()
+	}
+	return resp, nil
+}
+
+// append an "automated peering" entry to the end of bird output
+func peeringForm(query string, httpW http.ResponseWriter) {
+	if peeringConf == nil || query != "show protocols" {
+		return
+	}
+	httpW.Write([]byte(fmt.Sprintf(
+		"new_peer BGP automated open %s Peer with me in a minute!\n",
+		time.Now().Format("2006-01-02"),
+	)))
+}
